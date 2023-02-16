e429a4c4a8f15aff12b9da007463362a9ba5710b
breelef <emil_vinther@hotmail.com>
2023-02-16 12:43:45 +0100

    Birds API

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/birds", (req, res) => {
+    res.send({birds: "this is all birds"})
+});
+
+app.get("/birds/:birdType", (req, res) => {
+    res.send({birds: req.params.birdType})
+});
+
+app.listen(8080);
