0d792f39a712958a18df31786a2a3f20c54b7514
breelef <emil_vinther@hotmail.com>
2023-02-23 10:43:34 +0100

    Full crud birds API

--- app.js
+++ app.js
@@ -1,15 +1,43 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 app.use(express.json());
+const birds = [
+{id: 1, name: "Gråspurv"}
+];
+let idInc = 2;
 
 app.get("/birds", (req, res) => {
-    res.send({birds: "this is all birds"})
+    res.send(birds);
 });
 
 app.get("/birds/:birdType", (req, res) => {
     res.send({birds: req.params.birdType})
 
+});
+
+app.post("/birds", (req, res) => {
+    let bird = req.body;
+    bird.id = idInc;
+    idInc++;
+    birds.push(bird);
+    res.send(bird);
+});
+
+app.patch("/birds/:id", (req, res) => {
+    let birdToPatch = birds.find(bird => bird.id === Number(req.params.id)); 
+    let birdIndex = birds.indexOf(birdToPatch);
+    birds[birdIndex].name = req.body.name;
+    res.send(birds);
+
+});
+
+app.delete("/birds/:id", (req, res) => {
+    let birdToPatch = birds.find(bird => bird.id === Number(req.params.id)); 
+    let birdIndex = birds.indexOf(birdToPatch);
+    birds.splice(birdIndex, 1);
+    res.send(birds);
 });
 
 app.listen(8080);
